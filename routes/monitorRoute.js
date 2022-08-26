904c253f1cb8b12009302126b44a14b642408a6d
Ruturaj Doiphode <ruturaj.doiphode@capgemini.com>
2022-08-26 14:39:49 +0530

    handle notification mail trigger

--- routes/monitorRoute.js
+++ routes/monitorRoute.js
@@ -5,18 +5,33 @@ const nodemailer = require("nodemailer");
 const config = require("../config/config.json");
 
 router.get("/monitor", async (req, res) => {
+  let failedUrls = [];
   try {
     config.team_name.forEach((team) => {
-      team.urls_to_monitor.forEach((item) => {
-        request(item.url, (err, res) => {
-          if (res && res.statusCode === 200) {
-            console.log(item, "--->", res.statusCode);
-          } else {
-            triggerNotification(item, team.notification);
+      team.urls_to_monitor.forEach((item, index) => {
+        request(
+          {
+            url: item.url,
+            timeout: 10000,
+            followRedirect: true,
+            maxRedirects: 10,
+          },
+          (err, res) => {
+            if (res && res.statusCode === 200) {
+              console.log(item, "--->", res.statusCode);
+            } else {
+              failedUrls.push(item);
+              if (index === team.urls_to_monitor.length - 1) {
+                if (failedUrls.length > 0) {
+                  triggerNotification(failedUrls);
+                }
+              }
+            }
           }
-        });
+        );
       });
     });
+
     res.send("success");
   } catch (error) {
     console.log(error);
@@ -24,8 +39,8 @@ router.get("/monitor", async (req, res) => {
 });
 
 // async..await is not allowed in global scope, must use a wrapper
-const triggerNotification = async (item, recipients) => {
-  let users = recipients.toString();
+const triggerNotification = async (items) => {
+  let users = config.team_name[0].notification.toString();
   var transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -33,12 +48,16 @@ const triggerNotification = async (item, recipients) => {
       pass: "juehhrxovikbvdqe",
     },
   });
+  let body = "";
+  items.forEach((item) => {
+    body = body + `${item.name} : ${item.url} is down <br>`;
+  });
 
   var mailOptions = {
     from: "Dipayan Palit <dipayanpalit15@gmail.com>",
     to: users,
     subject: "Service Status",
-    text: `${item.name} : ${item.url} is down`,
+    html: body,
   };
   // console.log(item);
 
