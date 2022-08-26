50a380b44f25a56aaf46cfc1bd8d44da09604e9f
Ruturaj Doiphode <ruturaj.doiphode@capgemini.com>
2022-08-26 11:14:33 +0530

    create server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const app = express();
+dotenv.config({ path: "./.env" });
+const port = process.env.PORT;
+
+// const User = require('./Model/userSchema');
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello Eagles");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+});
